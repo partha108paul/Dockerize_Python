4139607f0b69686b8eb46c878f957561377c423d
partha108paul <102946785+partha108paul@users.noreply.github.com>
2023-03-24 00:12:16 +0530

    Dockerize_python

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def route():
+    return "Hello Bharat"
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=8080)
+
